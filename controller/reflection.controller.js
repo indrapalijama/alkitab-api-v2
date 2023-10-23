0f7243ff207b614b53e9e66db863a2de2278c663
Indra Palijama <indrapalijama@gmail.com>
2023-10-23 10:27:36 +0700

    add version params for reflection

--- controller/reflection.controller.js
+++ controller/reflection.controller.js
@@ -49,6 +49,63 @@ const get = async (req, res) => {
   });
 };
 
+const getCustom = async (req, res) => {
+  let url;
+  let version;
+  if (req.params.version === "sh") {
+    url = "https://alkitab.mobi/renungan/sh";
+    version = "Santapan Harian";
+  } else {
+    url = "https://alkitab.mobi/renungan/rh";
+    version = "Renungan Harian";
+  }
+  axios.get(url).then(({ data }) => {
+    let $ = cheerio.load(data);
+    var title = [];
+    var body = [];
+    var passage = [];
+    var date = [];
+
+    //begin cheerio scrapping
+    $("div").filter((i, el) => {
+      let data = $(el);
+      let strong = data.find("strong").first().text();
+      title.push(strong);
+      date.push(data.find("span").first().text());
+      passage.push(data.find("p").text().split(strong)[0]);
+      body.push(data.find("p").text().split(strong)[1]);
+    });
+
+    //filter out the undefined or empty string values
+    var filteredTitle = title.filter(function (el) {
+      return el != "";
+    });
+    var filteredBody = body.filter(function (el) {
+      return el != undefined;
+    });
+    var filteredPassage = passage.filter(function (el) {
+      return el != undefined;
+    });
+    var content = filteredBody[0].split("* * *")[0];
+    var tempFiltered = date.filter(function (el) {
+      return el != undefined;
+    });
+    var filteredTanggal = tempFiltered.filter(function (el) {
+      return el != "";
+    });
+
+    //return the data back
+    res.status(200).json({
+      Source: version,
+      Title: filteredTitle[0],
+      Date: filteredTanggal[2],
+      Passage: filteredPassage[0].split("Bacaan:")[1].trim(),
+      Content: content,
+    });
+  });
+};
+
 module.exports = {
   get,
+  getCustom,
 };
--- routes/reflection.route.js
+++ routes/reflection.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const reflection_controller = require("../controller/reflection.controller");
 
 router.get("/", reflection_controller.get);
+router.get("/:version", reflection_controller.getCustom);
 
 module.exports = router;
