98f4d9f59b0c652c04d3dac361cd2f4796bbae2a
Indra Palijama <indrapalijama@gmail.com>
2026-08-27 01:31:50 +0700

    fix: install missing @aws-sdk/s3-request-presigner

--- patch_index.py
+++
@@ -1,8 +0,0 @@
-with open('index.js', 'r') as f:
-    content = f.read()
-
-content = content.replace('var song = require("./routes/song.route");', 'var song = require("./routes/song.route");\nvar appRoute = require("./routes/app.route");')
-content = content.replace('app.use("/song", isAuth, song);', 'app.use("/song", isAuth, song);\napp.use("/app", appRoute); // Not auth required')
-
-with open('index.js', 'w') as f:
-    f.write(content)
